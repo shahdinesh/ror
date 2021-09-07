d3df6e58372e95f7182a9a23f1faba0c163cd5ec
dinesh <shahdinesh909@gmail.com>
2021-09-07 17:24:49 +0545

    using consern

---
+++ app/models/concerns/visible.rb
@@ -0,0 +1,11 @@
+module Visible
+    extend ActiveSupport::Concern
+    VALID_STATUSES = ['public', 'private', 'archived']
+    included do
+        validates :status, presence: true, inclusion: { in: VALID_STATUSES }
+    end
+
+    def archived?
+        status == 'archived'
+    end
+end
